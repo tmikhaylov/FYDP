8e1185feb10b506ba8133377a47159da109b2af2
Timur Mikhaylov <tmikhayl@uwaterloo.ca>
2025-03-06 19:32:19 -0500

    Change button color

--- frontend/app/(private-layout)/project/NewProjectPageClient.tsx
+++ frontend/app/(private-layout)/project/NewProjectPageClient.tsx
@@ -76,7 +76,7 @@ export default function NewProjectPageClient() {
           type="button"
           onClick={handleCreateProject}
           disabled={loading}
-          className="px-4 py-2 bg-sky-600 text-white rounded hover:bg-sky-500"
+          className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-sky-500"
         >
           {loading ? "Creating..." : "Create Project"}
         </button>
